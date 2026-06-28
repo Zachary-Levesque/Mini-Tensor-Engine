91511a17213b0ec1904f4145fbcfe9d15e8850d5
Zachary Levesque <zlevesque93@gmail.com>
2026-06-28 13:40:10 -0400

    Create cmake_clean.cmake

---
+++ build-x86-avx2/CMakeFiles/mte_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mte_benchmark.dir/src/benchmark.cpp.o"
+  "CMakeFiles/mte_benchmark.dir/src/benchmark.cpp.o.d"
+  "mte_benchmark"
+  "mte_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mte_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
