ce5411411ca3c8c8782835db245df3b9296fc9e0
Zachary Levesque <zlevesque93@gmail.com>
2026-06-28 13:39:08 -0400

    Create CTestTestfile.cmake

---
+++ build-x86-avx2/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine
+# Build directory: /Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/build-x86-avx2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(mte_tests "/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/build-x86-avx2/mte_tests")
+set_tests_properties(mte_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/CMakeLists.txt;56;add_test;/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/CMakeLists.txt;0;")
