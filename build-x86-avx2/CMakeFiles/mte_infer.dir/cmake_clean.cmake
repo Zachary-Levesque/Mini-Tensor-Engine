4d0c8c3196fd19cafda0ea80cccef8cc0aca8486
Zachary Levesque <zlevesque93@gmail.com>
2026-06-28 13:39:44 -0400

    Create cmake_clean.cmake

---
+++ build-x86-avx2/CMakeFiles/mte_infer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mte_infer.dir/src/main.cpp.o"
+  "CMakeFiles/mte_infer.dir/src/main.cpp.o.d"
+  "mte_infer"
+  "mte_infer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mte_infer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
