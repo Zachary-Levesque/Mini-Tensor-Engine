8a788374c64dad3ac9f2ca9fb82c8029217810da
Zachary Levesque <zlevesque93@gmail.com>
2026-06-28 13:40:32 -0400

    Create cmake_clean.cmake

---
+++ build-x86-avx2/CMakeFiles/mte.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mte.dir/src/io.cpp.o"
+  "CMakeFiles/mte.dir/src/io.cpp.o.d"
+  "CMakeFiles/mte.dir/src/layers.cpp.o"
+  "CMakeFiles/mte.dir/src/layers.cpp.o.d"
+  "CMakeFiles/mte.dir/src/model.cpp.o"
+  "CMakeFiles/mte.dir/src/model.cpp.o.d"
+  "CMakeFiles/mte.dir/src/tensor.cpp.o"
+  "CMakeFiles/mte.dir/src/tensor.cpp.o.d"
+  "libmte.a"
+  "libmte.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mte.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
