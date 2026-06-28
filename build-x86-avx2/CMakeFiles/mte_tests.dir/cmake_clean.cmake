960c240b04c21cf24cab65df0d0f7f4154c2f0a7
Zachary Levesque <zlevesque93@gmail.com>
2026-06-28 13:39:26 -0400

    Create cmake_clean.cmake

---
+++ build-x86-avx2/CMakeFiles/mte_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mte_tests.dir/tests/test_tensor.cpp.o"
+  "CMakeFiles/mte_tests.dir/tests/test_tensor.cpp.o.d"
+  "mte_tests"
+  "mte_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mte_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
