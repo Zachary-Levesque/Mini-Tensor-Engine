b98bece4d1bf4feede80a83bd65502fcd8dcf117
Zachary Levesque <zlevesque93@gmail.com>
2026-06-28 13:40:18 -0400

    Create DependInfo.cmake

---
+++ build-x86-avx2/CMakeFiles/mte.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/src/io.cpp" "CMakeFiles/mte.dir/src/io.cpp.o" "gcc" "CMakeFiles/mte.dir/src/io.cpp.o.d"
+  "/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/src/layers.cpp" "CMakeFiles/mte.dir/src/layers.cpp.o" "gcc" "CMakeFiles/mte.dir/src/layers.cpp.o.d"
+  "/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/src/model.cpp" "CMakeFiles/mte.dir/src/model.cpp.o" "gcc" "CMakeFiles/mte.dir/src/model.cpp.o.d"
+  "/Users/zacharylevesque/Documents/GitHub/Mini-Tensor-Engine/src/tensor.cpp" "CMakeFiles/mte.dir/src/tensor.cpp.o" "gcc" "CMakeFiles/mte.dir/src/tensor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
